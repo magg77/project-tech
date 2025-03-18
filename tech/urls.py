29defb060e92cf9a518b023d084500c1a23ce7c6
magg77 <escenariopaloma@gmail.com>
2025-03-18 05:39:58 -0500

    query native

--- tech/urls.py
+++ tech/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
-from tech.views import ProjectViewSet
+from tech.views import ProjectViewSet, ProjectListViewRaw, ProjectViewSetCursor
 
 router = routers.DefaultRouter()
 
 router.register("v1/projects", ProjectViewSet, "projects")
+router.register("v1/projects-raw", ProjectListViewRaw, "projects-raw")
+router.register("v1/projects-cursor", ProjectViewSetCursor, "projects-cursor")
 
 
 urlpatterns = router.urls
--- tech/views.py
+++ tech/views.py
@@ -1,12 +1,40 @@
+from django.db import connection
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 from tech.models import Project
 from tech.serializers import ProjectSerializer
 
-# Create your views here.
+# orm django
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [permissions.AllowAny]
 
 
+# .raw
+class ProjectListViewRaw(ViewSet):
+    def list(self, request):
 
+        # Ejecutar consulta SQL con .raw()
+        project = Project.objects.raw("SELECT * FROM public.tech_project ORDER BY id ASC")
+
+        # Convertir a lista para serializar correctamente
+        projects_list = list(project)  # Agregamos esta línea
+        
+        # Serializar los datos
+        serializer = ProjectSerializer(projects_list, many=True)  # Ahora serializa correctamente
+        
+        return Response(serializer.data)
+
+# cursor
+class ProjectViewSetCursor(ViewSet):
+    def list(self, request):  # En ViewSet se usa list(), no get()
+        query = "SELECT * FROM public.tech_project ORDER BY id ASC"
+        
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            columns = [col[0] for col in cursor.description]  # Obtener nombres de columnas
+            results = [dict(zip(columns, row)) for row in cursor.fetchall()]  # Convertir a diccionario
+        
+        return Response(results)  # Enviar JSON con los datos
